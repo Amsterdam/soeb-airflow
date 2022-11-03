986fcce505bbd3018baf2aac6a5ea97dc0d76bf7
“Pieter <p.d.smit@amsterdam.nl>
2022-11-03 14:26:32 +0100

    test5_pds

--- src/dags/azuretest_pieter.py
+++ src/dags/azuretest_pieter.py
@@ -35,7 +35,7 @@ with DAG(
     # 2. Create temp directories to store files
     make_temp_dirs = [
         BashOperator(
-            task_id=f"Make directory",
+            task_id=f"Make_directory",
             bash_command="mkdir -p {dirname}",
         )
     for dirname in createdirs
