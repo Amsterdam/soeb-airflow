bc80b68f8faf6327d94c4596c45a36f59c7c31a5
Bastiaan du Pon <b.du.pon@amsterdam.nl>
2022-11-07 21:49:15 +0100

    removed connections

--- src/dags/sql_test_bas.py
+++ src/dags/sql_test_bas.py
@@ -5,22 +5,8 @@ from airflow import DAG
 from common import MessageOperator, default_args, quote_string
 from contact_point.callbacks import get_contact_point_on_failure_callback
 from postgres_on_azure_operator import PostgresOnAzureOperator
-from sql.temp_table import CREATE_BAG_PND
-
 
 DAG_ID: Final = "sql_test_bas"
-
-# The local database connection.
-# This secret must exists in KV: `airflow-connections-soeb-postgres`
-# with the connection string present with protocol `postgresql://`
-# SOEB_DB_CONN_STRING: Final = Connection.get_connection_from_secrets(conn_id ="soeb_postgres" )
-SOEB_DB_CONN_STRING: Final = Variable.get("soeb_postgres")
-dsn_url = make_url(SOEB_DB_CONN_STRING)
-SOEB_HOST: Final = dsn_url.host
-SOEB_PORT: Final = 5432
-SOEB_USER: Final = dsn_url.username
-SOEB_PASSWD: Final = dsn_url.password
-SOEB_DBNAME: Final = dsn_url.database
 
 # DAG definition
 with DAG(
