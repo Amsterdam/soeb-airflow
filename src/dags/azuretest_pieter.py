b7b1bfe5f34e277e358d6a127eacb65e9b057b1d
“Pieter <p.d.smit@amsterdam.nl>
2022-11-07 13:40:53 +0100

    pds_test1

--- src/dags/azuretest_pieter.py
+++ src/dags/azuretest_pieter.py
@@ -58,23 +58,23 @@ with DAG(
 
     # Download waternet file from objectstore to work old
     download_waternetfile = SwiftOperator(
-        task_id=f"download_new_waternetfile_{osfilewaternet}",
+        task_id=f"download_{osfilewaternet}",
         swift_conn_id="OBJECTSTORE_WATERNET",
         action_type="download",
         container=f"{osfilelocation}",
         object_id=f"{osfilewaternet}",
-        output_path=f"{creadirs.get(3)}/{osfilewaternet}",
+        output_path=f"{creadirs.get(3)}",
     )
 
     # Zip new waternet file
     zip_new_waternetfile = BashOperator(
-        task_id=f"zip_to_{weeksdict.get(0)}",
+        task_id=f"zip_{weeksdict.get(0)}",
         bash_command=f"zip -q {creadirs.get(3)}/{weeksdict.get(0)} {creadirs.get(3)}/{osfilewaternet}" 
     )
     
     # Delete new kdrive object store
     delete_new_waternetfile = SwiftOperator(
-        task_id=f"delete_from_kdrive_os_{weeksdict.get(0)}",
+        task_id=f"delete_{weeksdict.get(0)}",
         swift_conn_id="OBJECTSTORE_WATERNET",
         action_type="delete",
         # container=f"{oskdrvfilelocation}",
@@ -82,6 +82,16 @@ with DAG(
         # container="geopackages",
         container="production",
         objects_to_del="Waternet_Assets_Levering_2022_week44.zip",
+    )
+
+    # Test Upload 
+    upload_waternetfile = SwiftOperator(
+        task_id=f"upload_{osfilewaternet}",
+        swift_conn_id="OBJECTSTORE_WATERNET",
+        action_type="upload",
+        container=f"production",
+        object_id="Waternet_Assets_Levering_2022_week44.zip",
+        output_path=f"{creadirs.get(3)}",
     )
 
     # delete old files from objectsore
